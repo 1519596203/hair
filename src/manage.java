9d47fff0ef65914684a384108dac98b1c27b3071
仔 靓 <1519596203@qq.com>
2022-05-26 20:25:50 +0800

    Init

--- src/manage.java
+++ src/manage.java
@@ -1,5 +1,5 @@
 public class manage {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//sdgsdg
 //        int nn;
 //        Scanner READER = new Scanner(System.in);
 //        nn = READER.nextInt();
